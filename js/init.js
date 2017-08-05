9f175905423489859fce5a0e7d036fbfd2e79a95
BrainStation <brainstation@BrainStations-MacBook-Air.local>
2017-08-05 10:28:56 -0400

    update

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.carousel').carousel({duration: 200});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
